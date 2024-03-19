715564208a1237ebc96e631b3bae5de4313a832f
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-03-19 13:49:19 +0100

    Created Item script and polished some minor settings

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // we need to create a canvas and put the item under that canvas so it can be freely moved
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //We need to assign the pointer position to this item
+    }
+}
