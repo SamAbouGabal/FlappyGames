3ad74954180b29cc1fd0337e3ace7fb596a96c08
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-03-19 12:04:16 +0100

    Update

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public Image itemPrefab; 
+    public Color[] itemColors; 
+
+    private RectTransform contentRect; 
+
+    void Start()
+    {
+        if (itemPrefab == null || itemColors.Length == 0)
+        {
+            Debug.LogError("Please assign item prefab and color array!");
+            return;
+        }
+
+        contentRect = GetComponentInChildren<ScrollRect>().content; 
+
+        // Loop through colors and instantiate items
+        for (int i = 0; i < itemColors.Length; i++)
+        {
+            Image newItem = Instantiate(itemPrefab, contentRect); // Instantiate inside Scroll Rect's content
+            newItem.color = itemColors[i]; // Set the color of the new item
+        }
+    }
+}
